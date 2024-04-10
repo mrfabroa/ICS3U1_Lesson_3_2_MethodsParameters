9dc5957a4a1c2a08fbb91ec3928afb411d6d065c
mrfabroa <eric.fabroa@ycdsbk12.ca>
2024-04-10 14:08:49 -0400

    lesson updates

--- Program1.java
+++ Program1.java
@@ -6,20 +6,37 @@ class Program1 extends ConsoleProgram {
   */
   
   public void run(){
-    System.out.println("Happy Birthday to you!");
-    System.out.println("Happy Birthday to you!");
-    System.out.println("Happy birthday, dear Fred!");
-    System.out.println("Happy Birthday to you!");
-}
+
+    String strCelebrant;
+
+    strCelebrant = readLine("Who's birtday is it? ");
+
+    happy();
+    happy();
+    dear(strCelebrant);
+    happy();
+
+    area(5,4);
+  }
 
   /**
    * Say the happy birthday line
    */
- 
+  private void happy(){
+    System.out.println("Happy Birthday to you!");
+  }
+
 
   /**
   * output happy birthday to a specified name
   * @param name - The name of the person to sing to
   */
+  private void dear(String strName){
+    System.out.println("Happy Birthday dear " + strName + "!");
+  }
+
+  private void area(int length, int width){
+    System.out.println(length * width);
+  }
 
 }
